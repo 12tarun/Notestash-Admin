9d34a2a7838bc39f5c320562ec6e1d0007785034
Tarun Gupta <12tarungupta@gmail.com>
2018-03-06 23:18:54 +0530

    display user data

---
+++ Notestash-Admin/Controllers/UserDataController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Notestash_Admin.Controllers
+{
+    public class UserDataController : Controller
+    {
+        // GET: UserData
+        public ActionResult User_Data()
+        {
+            using (Notestash_Database_Entities db = new Notestash_Database_Entities())
+            {
+                var users = db.tblUsers.ToList();
+                return View(users);
+            }             
+        }
+    }
+}
---
+++ Notestash-Admin/Models/userData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Notestash_Admin.Models
+{
+    public class userData
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public byte[] ProfilePicture { get; set; }
+        public int Status { get; set; }
+    }
+}
