e961a1e7e4508aaec75f737ffe1850b57e08cfd9
Tarun Gupta <12tarungupta@gmail.com>
2018-04-07 20:54:52 +0530

    add forgot password

--- Notestash-Admin/Controllers/LoginController.cs
+++ Notestash-Admin/Controllers/LoginController.cs
@@ -10,6 +10,9 @@ using PBKDF2 = SecurityDriven.Inferno.Kdf.PBKDF2;
 using SecurityDriven.Inferno.Extensions;
 using static SecurityDriven.Inferno.SuiteB;
 using static SecurityDriven.Inferno.Utils;
+using System.Net.Mail;
+using System.Net;
+using SecurityDriven.Inferno;
 
 namespace Notestash_Admin.Controllers
 {
@@ -62,7 +65,9 @@ namespace Notestash_Admin.Controllers
                         if (userCredentials != null)
                         {
                             Session["Login"] = user.Email;
-                            //ViewBag.Login = Session["Login"];
+                            
+                            // cookie based login
+
                             //int timeout = User.RememberMe ? 52560 : 20;
                             //var ticket = new FormsAuthenticationTicket(User.Email, User.RememberMe, timeout);
                             //string encrypted = FormsAuthentication.Encrypt(ticket);
@@ -90,6 +95,122 @@ namespace Notestash_Admin.Controllers
             }
             return View();
         }
+
+        [HttpGet]
+        public ActionResult forgotPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult forgotPassword(forgotPassword User)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("BadRequest", "Registration failed!");
+            }
+                
+            try
+            {
+                using (Notestash_Database_Entities db = new Notestash_Database_Entities())
+                {
+                    var emailId = db.tblUsers.FirstOrDefault(e => e.Email == User.Email);
+                    emailId.forgotPasswordCode = Guid.NewGuid();
+                    db.SaveChanges();
+                    changePasswordEmail(emailId.Email, emailId.forgotPasswordCode.ToString());
+                    ModelState.AddModelError("Sent", "Link to change password has been sent to your email id.");
+                }
+            }
+            catch (Exception ex)
+            {
+                string s = ex.Message;
+                ModelState.AddModelError("BadRequest", "Registration failed!");
+            }
+            return View();
+        }
+
+        [NonAction]
+        public void changePasswordEmail(string Email, string passwordChangeKey)
+        {
+            var verifyUrl = "/Login/changePassword/" + passwordChangeKey;
+        //  Uri link = new Uri(Request.RequestUri.AbsoluteUri.Replace(Request.RequestUri.PathAndQuery, verifyUrl));
+            var link = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, verifyUrl);
+            var fromEmail = new MailAddress("suryakant.rocky@gmail.com", "Notestash");
+            var toEmail = new MailAddress(Email);
+            var fromEmailPassword = "suryasharma";
+            string subject = "Change Password";
+            string body = "<br/><br/>Please click on the link below to change your password." + "<br/><br/><a href='" + link + "'>" + link + "</a>";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword),
+            };
+
+            using (var message = new MailMessage(fromEmail, toEmail)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            })
+                smtp.Send(message);
+        }
+
+        [HttpGet]
+        public ActionResult changePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult changePassword(string id, changePassword pass)
+        {
+            try
+            {
+                using (Notestash_Database_Entities db = new Notestash_Database_Entities())
+                {
+                    var passwordChanged = db.tblUsers.Where(e => e.forgotPasswordCode == new Guid(id)).FirstOrDefault();
+                    string newPass = pass.newPassword;
+
+                    var sha384Factory = HmacFactory;
+                    var random = new CryptoRandom();
+
+                    byte[] derivedKey;
+                    string hashedPassword = null;
+                    string passwordText = newPass;
+
+                    byte[] passwordBytes = SafeUTF8.GetBytes(passwordText);
+                    var salt = random.NextBytes(384 / 8);
+
+                    using (var pbkdf2 = new PBKDF2(sha384Factory, passwordBytes, salt, 256 * 1000))
+                        derivedKey = pbkdf2.GetBytes(384 / 8);
+
+
+                    using (var hmac = sha384Factory())
+                    {
+                        hmac.Key = derivedKey;
+                        hashedPassword = hmac.ComputeHash(passwordBytes).ToBase16();
+                    }
+
+                    passwordChanged.Password = hashedPassword;
+                    passwordChanged.Salt = salt;
+                    passwordChanged.forgotPasswordCode = null;
+                    db.SaveChanges();
+                 //   return Request.CreateResponse(HttpStatusCode.OK, "Password changed successfully!");
+                }
+            }
+            catch (Exception ex)
+            {
+                string s = ex.Message;
+               // return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Error occurred, please try again!");
+            }
+            return View();
+        }
+
 
         // Logout
         public ActionResult SignOut()
