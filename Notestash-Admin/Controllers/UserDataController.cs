2929d89af602a84cfe94ee55173afad225eacc9a
Tarun Gupta <12tarungupta@gmail.com>
2018-04-08 22:28:05 +0530

    fix bugs

--- Notestash-Admin/Controllers/UserDataController.cs
+++ Notestash-Admin/Controllers/UserDataController.cs
@@ -9,11 +9,13 @@ namespace Notestash_Admin.Controllers
 {
     public class UserDataController : Controller
     {
-
+        
      // Show users' data
         [HttpGet]
         public ActionResult User_Data()
         {
+            Session["CannotDelete"] = null;
+
             if(Session["Login"] == null)
             {
                 return RedirectToAction("SignIn", "Login");
@@ -39,6 +41,11 @@ namespace Notestash_Admin.Controllers
             {
                 using (Notestash_Database_Entities db = new Notestash_Database_Entities())
                 {
+                    if (Session["CannotDelete"] != null)
+                    {
+                        ModelState.AddModelError("CannotDelete", "Admin cannot delete himself!");
+                    }
+
                     var selectUser = db.tblUsers.Where(e => e.Id == id).FirstOrDefault();
                     return View(selectUser);
                 }
@@ -56,16 +63,17 @@ namespace Notestash_Admin.Controllers
             {
                 using (Notestash_Database_Entities db = new Notestash_Database_Entities())
                 {
-                    if(id != (int)Session["Login"])                                                // User cannot delete himself as there should be atleast one admin                                                                                                  
-                    {                                                                              // if he deletes all other admins.
-                        var deleteUser = db.tblUsers.Where(e => e.Id == id).FirstOrDefault();      ///////////////
+                    var deleteUser = db.tblUsers.Where(e => e.Id == id).FirstOrDefault();
+                    if (id != (int)Session["Login"])                                                // User cannot delete himself as there should be atleast one admin                                                                                                  
+                    {                                                                               // if he deletes all other admins.                    
                         db.tblUsers.Remove(deleteUser);
                         db.SaveChanges();
                         return RedirectToAction("User_Data", "UserData");
                     }
                     else
                     {
-                        return RedirectToAction("User_Data", "UserData");
+                        Session["CannotDelete"] = 1;
+                        return RedirectToAction("SelectedUser", "UserData", new { id = deleteUser.Id });
                     }
                 }
             }
